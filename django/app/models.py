32d47aa42e14bea9465e332d73898d347c61fde6
mohammadi-nia <endalyban@gmail.com>
2023-05-09 16:11:21 +0330

    UserProfileModel created

--- django/app/models.py
+++ django/app/models.py
@@ -1,3 +1,59 @@
 from django.db import models
+from django.core.validators import RegexValidator, MinValueValidator
+from django.contrib.auth.models import BaseUserManager, PermissionsMixin, AbstractBaseUser
 
-# Create your models here.
+
+
+# classes
+class UserProfileManager(BaseUserManager):
+    """
+    کلاس منیجر کاربر
+    """
+    def create_user(self ,first_name, last_name, mobile_number, password = None):
+        """
+        برای ذخیره کاربر عادی
+        """
+        user = self.model(first_name = first_name, last_name = last_name, mobile_number = mobile_number)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, first_name, last_name, mobile_number, password):
+        """
+        برای ذخیره کاربر ادمین
+        """
+        user = self.create_user(first_name, last_name, mobile_number, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user
+
+
+class UserProfileModel(AbstractBaseUser , PermissionsMixin):
+    """
+    مدل کاربر
+    """
+    first_name = models.CharField(max_length = 25 , null = True, default = None) # نام کاربر
+    last_name = models.CharField(max_length = 25 , null = True, default = None) # نام خانوادگی کاربر
+    mobile_number = models.CharField(max_length = 11 ,unique = True ,null = True ,validators=[
+        RegexValidator(
+            regex='^[0][9][0-9]*$',
+            message='قالب شماره موبایل رعایت نشده است',
+            code='invalid_phone_number'
+        ),
+    ]) # شماره تماس کاربر
+
+
+    is_staff = models.BooleanField(default=False) # کابر میتواند پنل ادمین را ببیند
+    is_active = models.BooleanField(default=True) # ایا کاربر فعال است
+    # کلاس منیجر
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = 'mobile_number' # نام کاربری
+    REQUIRED_FIELDS = ['first_name', 'last_name'] # فیلد های تکراری
+
+    def __str__(self):
+
+        return str(self.id) + ' - ' + str(self.mobile_number)
