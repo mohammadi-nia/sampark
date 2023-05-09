407d0c3748dcc035ea855efcc617effe2513d5d1
mohammadi-nia <endalyban@gmail.com>
2023-05-09 20:45:01 +0330

    some models registerd in

--- django/app/admin.py
+++ django/app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from app import models
 
-# Register your models here.
+
+
+admin.site.register(models.ParkingModel)
+admin.site.register(models.ParkingSpaceFloorModel)
+admin.site.register(models.ParkingSpaceRowModel)
+admin.site.register(models.ParkingSpaceNumberModel)
+admin.site.register(models.TicketModel)
+admin.site.register(models.UserProfileModel)
